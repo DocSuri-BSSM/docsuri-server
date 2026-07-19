d9e03f1cffaa41628398f09b76114cf035dd2964
rhtnddd <qlalfql123@gmail.com>
2026-07-19 18:28:04 +0900

    Initial commit

---
+++ src/main/java/com/example/docsuriserver/DocsuriServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.docsuriserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsuriServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocsuriServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/docsuriserver/DocsuriServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docsuriserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsuriServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
